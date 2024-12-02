5c9085046c4356853e4eb3dabea1ba50f5e78116
Jorintas Butkus <joriskoris@protonmail.com>
2024-12-02 16:40:56 -0600

    moar

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -6,4 +6,15 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
+map("n", "<leader>dx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostic (Trouble)" })
+map("n", "<leader>dX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer diagnostics (Trouble)" })
+map("n", "<leader>dQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix list (Trouble)" })
+map("n", "<leader>pc", function()
+  if require("precognition").toggle() then
+    vim.notify "precognition on"
+  else
+    vim.notify "precognition off"
+  end
+end, { desc = "Precognition toggle " })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,16 +1,27 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre",
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
+  {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+  },
+  {
+    "tris203/precognition.nvim",
+    opts = {},
+  },
+  {
+    "ggandor/leap.nvim",
+    opts = {},
+  },
 }
